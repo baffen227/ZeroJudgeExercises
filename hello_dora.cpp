a63b26b32627e72bce50688a8962837a4b972972
baffen227 <baffen227@gmail.com>
2023-09-01 09:17:55 +0800

    Update folder structures

--- hello_dora.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, 朵拉!!!!!\n";
-    int n; cin >> n;
-    for (int i = 0; i < n; i++)
-        cout << i+1 << "是小屁屁\n";
-}
--- test_tmp_input.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    int input_data;
-    while (std::cin >> input_data)
-        std::cout << input_data << '\n';
-}
